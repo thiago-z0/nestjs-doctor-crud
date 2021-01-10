07694cd5730ad9890764e2d229219732e681718c
Thiago Ribeiro <thiago.correia@gcbinvestinentos.com>
2021-01-10 16:52:54 -0300

    Progresso docker compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('Nest.js Doctor Crud')
+  .setDescription('Documentação da API Doctor Crud feita em Nest.js')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.API_PORT);
   console.log('**Working on %s**', process.env.API_PORT);
 }
